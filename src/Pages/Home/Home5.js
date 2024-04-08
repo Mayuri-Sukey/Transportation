3eb624c3092a77e0fcba29a3355ed85f826da436
Chaten Mundle <chetanmundle7875@gmail.com>
2024-04-08 11:39:48 +0530

    c

--- src/Pages/Home/Home5.js
+++ src/Pages/Home/Home5.js
@@ -1,77 +1,12 @@
-import React from "react";
+import React from 'react'
 import { IoMailSharp } from "react-icons/io5";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
+
 const Home5 = () => {
   return (
     <>
-<<<<<<< HEAD
-      <section className="Contact_US">
-        <div className="my-6">
-          <div class="max-w-6xl mx-auto bg-[#F0F8FF] rounded-lg">
-            <div class="grid md:grid-cols-2 items-center gap-16 sm:p-10 p-4 font-[sans-serif]">
-              <div>
-                <h1 class="text-4xl font-extrabold text-black">Get in Touch</h1>
-                <p class="text-md text-gray-900 mt-3">
-                  Get in touch with us! We welcome your inquiries, feedback, and
-                  collaboration ideas. Whether you have questions about our
-                  products or need assistance, our team is here for you. Connect
-                  with us through the provided contact details, and we look
-                  forward to hearing from you soon.
-                </p>
-                <ul class="mt-12 space-y-8">
-                  <li class="flex items-center">
-                    <IoMailSharp />
-                    <a
-                      href="javascript:void(0)"
-                      class="text-black text-sm ml-3"
-                    >
-                      <strong>info@mhnochanwala.com</strong>
-                    </a>
-                  </li>
-                  <li class="flex items-center">
-                    <FaPhoneAlt />
-                    <a
-                      href="javascript:void(0)"
-                      class="text-black text-sm ml-3"
-                    >
-                      <strong> +91 9307091009</strong>
-                    </a>
-                  </li>
-                  <li class="flex items-center">
-                    <FaLocationDot />
-                    <a
-                      href="javascript:void(0)"
-                      class="text-black text-sm ml-3"
-                    >
-                      <strong>
-                        Regd. Office: MH Nochanwala Exporter, City Kamptee, Pin
-                        Code 441001,District Nagpur, Maharashtra (State), INDIA
-                        Social Media Integration Facebook, Instagram, Youtube,
-                        Twitter
-                      </strong>
-                    </a>
-                  </li>
-                </ul>
-                <ul class="flex mt-12 space-x-4">
-                  <li class="bg-[blue] hover:bg-[#a91079e2] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                    <a href="javascript:void(0)">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18px"
-                        height="18px"
-                        fill="white"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          d="M6.812 13.937H9.33v9.312c0 .414.335.75.75.75l4.007.001a.75.75 0 0 0 .75-.75v-9.312h2.387a.75.75 0 0 0 .744-.657l.498-4a.75.75 0 0 0-.744-.843h-2.885c.113-2.471-.435-3.202 1.172-3.202 1.088-.13 2.804.421 2.804-.75V.909a.75.75 0 0 0-.648-.743A26.926 26.926 0 0 0 15.071 0c-7.01 0-5.567 7.772-5.74 8.437H6.812a.75.75 0 0 0-.75.75v4c0 .414.336.75.75.75zm.75-3.999h2.518a.75.75 0 0 0 .75-.75V6.037c0-2.883 1.545-4.536 4.24-4.536.878 0 1.686.043 2.242.087v2.149c-.402.205-3.976-.884-3.976 2.697v2.755c0 .414.336.75.75.75h2.786l-.312 2.5h-2.474a.75.75 0 0 0-.75.75V22.5h-2.505v-9.312a.75.75 0 0 0-.75-.75H7.562z"
-                          data-original="#000000"
-                        />
-                      </svg>
-                    </a>
-                  </li>
-=======
     <section className='Contact_US'>
       <div className ="my-6">
             <div class="max-w-6xl mx-auto bg-[#F0F8FF] rounded-lg">
@@ -127,125 +62,59 @@ Social Media Integration Facebook, Instagram, Youtube, Twitter</strong>
                                     </svg>
                                 </a>
                             </li>
->>>>>>> 5cdb190088a15d1234d301504c52c2f6945eac02
 
-                  <li class="bg-[#da214f] hover:bg-[#da214f] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                    <a href="javascript:void(0)">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18px"
-                        height="18px"
-                        fill="#fff"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 9.3a2.7 2.7 0 1 0 0 5.4 2.7 2.7 0 0 0 0-5.4Zm0-1.8a4.5 4.5 0 1 1 0 9 4.5 4.5 0 0 1 0-9Zm5.85-.225a1.125 1.125 0 1 1-2.25 0 1.125 1.125 0 0 1 2.25 0ZM12 4.8c-2.227 0-2.59.006-3.626.052-.706.034-1.18.128-1.618.299a2.59 2.59 0 0 0-.972.633 2.601 2.601 0 0 0-.634.972c-.17.44-.265.913-.298 1.618C4.805 9.367 4.8 9.714 4.8 12c0 2.227.006 2.59.052 3.626.034.705.128 1.18.298 1.617.153.392.333.674.632.972.303.303.585.484.972.633.445.172.918.267 1.62.3.993.047 1.34.052 3.626.052 2.227 0 2.59-.006 3.626-.052.704-.034 1.178-.128 1.617-.298.39-.152.674-.333.972-.632.304-.303.485-.585.634-.972.171-.444.266-.918.299-1.62.047-.993.052-1.34.052-3.626 0-2.227-.006-2.59-.052-3.626-.034-.704-.128-1.18-.299-1.618a2.619 2.619 0 0 0-.633-.972 2.595 2.595 0 0 0-.972-.634c-.44-.17-.914-.265-1.618-.298-.993-.047-1.34-.052-3.626-.052ZM12 3c2.445 0 2.75.009 3.71.054.958.045 1.61.195 2.185.419A4.388 4.388 0 0 1 19.49 4.51c.457.45.812.994 1.038 1.595.222.573.373 1.227.418 2.185.042.96.054 1.265.054 3.71 0 2.445-.009 2.75-.054 3.71-.045.958-.196 1.61-.419 2.185a4.395 4.395 0 0 1-1.037 1.595 4.44 4.44 0 0 1-1.595 1.038c-.573.222-1.227.373-2.185.418-.96.042-1.265.054-3.71.054-2.445 0-2.75-.009-3.71-.054-.958-.045-1.61-.196-2.185-.419A4.402 4.402 0 0 1 4.51 19.49a4.414 4.414 0 0 1-1.037-1.595c-.224-.573-.374-1.227-.419-2.185C3.012 14.75 3 14.445 3 12c0-2.445.009-2.75.054-3.71s.195-1.61.419-2.185A4.392 4.392 0 0 1 4.51 4.51c.45-.458.994-.812 1.595-1.037.574-.224 1.226-.374 2.185-.419C9.25 3.012 9.555 3 12 3Z"></path>
-                      </svg>
-                    </a>
-                  </li>
+                            <li class="bg-[#007aff] hover:bg-[#007aff] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
+    <a href="https://twitter.com" target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#fff" viewBox="0 0 24 24">
+            <path d="M12 2.25c5.799 0 10.5 4.701 10.5 10.5s-4.701 10.5-10.5 10.5-10.5-4.701-10.5-10.5 4.701-10.5 10.5-10.5zm0-2.25c-6.624 0-12 5.376-12 12s5.376 12 12 12 12-5.376 12-12-5.376-12-12-12zm5.25 8.689c.005.136.008.271.008.411 0 4.146-3.15 8.914-8.914 8.914-1.772 0-3.412-.514-4.801-1.399.244.028.49.043.739.043 1.456 0 2.797-.494 3.853-1.322a3.155 3.155 0 0 1-2.937-2.186c.191.036.384.057.583.057.283 0 .558-.037.821-.107a3.154 3.154 0 0 1-2.526-3.092v-.04c.42.231.9.371 1.413.387a3.152 3.152 0 0 1-.981-4.213 8.949 8.949 0 0 0 6.504 3.296 3.144 3.144 0 0 1-.08-.746 3.15 3.15 0 0 1 3.15-3.15 3.144 3.144 0 0 1 2.326 1.029 6.308 6.308 0 0 0 1.994-.761 3.146 3.146 0 0 1-1.387 1.739c.636-.076 1.247-.243 1.812-.493a6.993 6.993 0 0 0-1.556 1.612z"></path>
+        </svg>
+    </a>
+</li>
 
-                  <li class="bg-[#007aff] hover:bg-[#007aff] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                    <a href="https://twitter.com" target="_blank">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18px"
-                        height="18px"
-                        fill="#fff"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 2.25c5.799 0 10.5 4.701 10.5 10.5s-4.701 10.5-10.5 10.5-10.5-4.701-10.5-10.5 4.701-10.5 10.5-10.5zm0-2.25c-6.624 0-12 5.376-12 12s5.376 12 12 12 12-5.376 12-12-5.376-12-12-12zm5.25 8.689c.005.136.008.271.008.411 0 4.146-3.15 8.914-8.914 8.914-1.772 0-3.412-.514-4.801-1.399.244.028.49.043.739.043 1.456 0 2.797-.494 3.853-1.322a3.155 3.155 0 0 1-2.937-2.186c.191.036.384.057.583.057.283 0 .558-.037.821-.107a3.154 3.154 0 0 1-2.526-3.092v-.04c.42.231.9.371 1.413.387a3.152 3.152 0 0 1-.981-4.213 8.949 8.949 0 0 0 6.504 3.296 3.144 3.144 0 0 1-.08-.746 3.15 3.15 0 0 1 3.15-3.15 3.144 3.144 0 0 1 2.326 1.029 6.308 6.308 0 0 0 1.994-.761 3.146 3.146 0 0 1-1.387 1.739c.636-.076 1.247-.243 1.812-.493a6.993 6.993 0 0 0-1.556 1.612z"></path>
-                      </svg>
-                    </a>
-                  </li>
+<li class="bg-[red] hover:bg-[red] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
+    <a href="https://youtube.com" target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#fff" viewBox="0 0 24 24">
+            <path d="M12 3c-2.56 0-5.12.12-7.68.44C2.51 3.61 2 5.6 2 7.99v8.02c0 2.39.51 4.38 2.32 4.54 2.56.32 5.12.44 7.68.44 2.56 0 5.12-.12 7.68-.44 1.81-.16 2.32-2.15 2.32-4.54V7.99c0-2.39-.51-4.38-2.32-4.54-2.56-.32-5.12-.44-7.68-.44zM9.91 15.23V8.77L15.67 12l-5.76 3.23z"></path>
+        </svg>
+    </a>
+</li>
 
-<<<<<<< HEAD
-                  <li class="bg-[red] hover:bg-[red] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                    <a href="https://youtube.com" target="_blank">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18px"
-                        height="18px"
-                        fill="#fff"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 3c-2.56 0-5.12.12-7.68.44C2.51 3.61 2 5.6 2 7.99v8.02c0 2.39.51 4.38 2.32 4.54 2.56.32 5.12.44 7.68.44 2.56 0 5.12-.12 7.68-.44 1.81-.16 2.32-2.15 2.32-4.54V7.99c0-2.39-.51-4.38-2.32-4.54-2.56-.32-5.12-.44-7.68-.44zM9.91 15.23V8.77L15.67 12l-5.76 3.23z"></path>
-                      </svg>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div class="bg-[#F7BE27] p-6 rounded-lg ">
-                <p class="text-lg font-semibold text-[#333] text-center">
-                  Unlock Customized Solutions <br></br> Submit Your Inquiry Form
-                  Today!
-                </p>
-                {/* <div class="space-y-4 max-lg:mt-4">
-=======
 
                         </ul>
                     </div>
                     <div class="bg-[#D6A627] p-6 rounded-lg ">
                     <p class="text-lg font-semibold text-[#333] text-center">Unlock Customized Solutions <br></br> Submit Your Inquiry Form Today!</p>
                         {/* <div class="space-y-4 max-lg:mt-4">
->>>>>>> 5cdb190088a15d1234d301504c52c2f6945eac02
                             <button type="button" class="px-4 py-2 rounded-md bg-[#a91079] text-white text-sm tracking-wider font-medium outline-none border-2 border-[#a91079] mr-4">Web design</button>
                             <button type="button" class="px-4 py-2 rounded-md bg-transparent text-gray-400 text-sm tracking-wider font-medium outline-none border-2 border-gray-300 mr-4">Graphic design</button>
                             <button type="button" class="px-4 py-2 rounded-md bg-transparent text-gray-400 text-sm tracking-wider font-medium outline-none border-2 border-gray-300">Design system</button>
                         </div> */}
-                <form class="mt-8 space-y-4">
-                  <input
-                    type="text"
-                    placeholder="Your Name"
-                    class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]"
-                  />
-                  <input
-                    type="email"
-                    placeholder="Business Name"
-                    class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Mobile Number"
-                    class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Email ID"
-                    class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]"
-                  />
-                  <textarea
-                    placeholder="Message"
-                    rows="6"
-                    class="w-full rounded-md px-4 text-sm pt-3 outline-[#51abae] resize-none"
-                  ></textarea>
-                  <button
-                    type="button"
-                    class="text-white bg-gray-700 hover:bg-[#51abae] font-semibold rounded-md text-sm px-4 py-3 flex items-center justify-center w-full"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16px"
-                      height="16px"
-                      fill="#fff"
-                      class="mr-2"
-                      viewBox="0 0 548.244 548.244"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M392.19 156.054 211.268 281.667 22.032 218.58C8.823 214.168-.076 201.775 0 187.852c.077-13.923 9.078-26.24 22.338-30.498L506.15 1.549c11.5-3.697 24.123-.663 32.666 7.88 8.542 8.543 11.577 21.165 7.879 32.666L390.89 525.906c-4.258 13.26-16.575 22.261-30.498 22.338-13.923.076-26.316-8.823-30.728-22.032l-63.393-190.153z"
-                        clip-rule="evenodd"
-                        data-original="#000000"
-                      />
-                    </svg>
-                    Send Message
-                  </button>
-                </form>
-              </div>
+                        <form class="mt-8 space-y-4">
+                            <input type='text' placeholder='Your Name'
+                                class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]" />
+                            <input type='email' placeholder='Business Name'
+                                class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]" />
+                            <input type='text' placeholder='Mobile Number'
+                                class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]" />
+                             <input type='text' placeholder='Email ID'
+                                class="w-full rounded-md py-3 px-4 text-sm outline-[#51abae]" />
+                            <textarea placeholder='Message' rows="6"
+                                class="w-full rounded-md px-4 text-sm pt-3 outline-[#51abae]"></textarea>
+                            <button type='button'
+                                class="text-white bg-gray-700 hover:bg-[#51abae] font-semibold rounded-md text-sm px-4 py-3 flex items-center justify-center w-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill='#fff' class="mr-2" viewBox="0 0 548.244 548.244">
+                                    <path fill-rule="evenodd" d="M392.19 156.054 211.268 281.667 22.032 218.58C8.823 214.168-.076 201.775 0 187.852c.077-13.923 9.078-26.24 22.338-30.498L506.15 1.549c11.5-3.697 24.123-.663 32.666 7.88 8.542 8.543 11.577 21.165 7.879 32.666L390.89 525.906c-4.258 13.26-16.575 22.261-30.498 22.338-13.923.076-26.316-8.823-30.728-22.032l-63.393-190.153z" clip-rule="evenodd" data-original="#000000" />
+                                </svg>
+                                Send Message
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </section>
+    </section>
     </>
-  );
-};
+  )
+}
 
-export default Home5;
+export default Home5
